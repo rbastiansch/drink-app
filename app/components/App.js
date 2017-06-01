283579dacf33c2cdf0a6abeb15c2fb87c3a2dc1b
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2017-06-01 01:17:19 -0300

    added bootstrap to be used as homepage

--- app/components/App.js
+++ app/components/App.js
@@ -4,9 +4,15 @@ import { Link } from 'react-router';
 class App extends Component {
 	render(){
 		return(
-			<div>
-				<Link to='/'>Home</Link>
-				<Link to='/section'>Section</Link>
+			<div className="container">
+				<div className="jumbotron jumbotron-fluid">
+				  <div className="container">
+				    <h1 className="display-3 d-flex justify-content-center">
+				    	<Link to="/" className="nav-link">Drink APP</Link>
+				    </h1>
+				    <p className="lead d-flex justify-content-center">This is an application to search some beers from an API.</p>
+				  </div>
+				</div>
 				{this.props.children}
 			</div>
 		);
